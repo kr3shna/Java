71a4e06362d2a36c2a9f5e65b8487b90fb3ac624
kr3shna <krishna2shende@gmail.com>
2024-08-10 23:58:48 +0530

    Pattern7

---
+++ patternPrinting/Pattern7/pattern7.java
@@ -0,0 +1,14 @@
+package Pattern7;
+import java.util.*;
+public class pattern7 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= 2 * i - 1; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
