31bafaab4f1593426a41caec41f74c0526852168
kr3shna <krishna2shende@gmail.com>
2024-08-10 21:12:46 +0530

    Pattern1

---
+++ Pattern1/pattern1.java
@@ -0,0 +1,14 @@
+package Pattern1;
+import java.util.*;
+public class pattern1 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
