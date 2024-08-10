73dea459a9dd193e438aacaef3bf9ee2a8e9ca03
kr3shna <krishna2shende@gmail.com>
2024-08-10 23:17:48 +0530

    Pattern4

---
+++ Pattern4/pattern4.java
@@ -0,0 +1,20 @@
+package Pattern4;
+import java.util.*;
+public class pattern4 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 2; j <= 5 - i + 1; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
