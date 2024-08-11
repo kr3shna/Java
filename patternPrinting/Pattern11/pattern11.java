342b9533c1bc5e31f7703a29b6799855b522b983
kr3shna <krishna2shende@gmail.com>
2024-08-11 18:42:42 +0530

    Pattern11

---
+++ patternPrinting/Pattern11/pattern11.java
@@ -0,0 +1,17 @@
+package Pattern11;
+import java.util.*;
+public class pattern11 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int k = 1; k <= n; k++){
+                System.out.print("-");
+            }
+            for(int j = 1; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
