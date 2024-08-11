5c1e1b07847051e1d0634371bbd8cf8e4093f5cd
kr3shna <krishna2shende@gmail.com>
2024-08-11 10:03:54 +0530

    Pattern10

---
+++ patternPrinting/Pattern10/pattern10.java
@@ -0,0 +1,15 @@
+package Pattern10;
+import java.util.*;
+public class pattern10 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= n; i++){
+            System.out.print("-");
+            for(int j =1; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
