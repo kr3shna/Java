6836379a687a0297c358ebf3856f1dfbf4bb24b3
kr3shna <krishna2shende@gmail.com>
2024-08-11 09:45:23 +0530

    Pattern8

---
+++ patternPrinting/Pattern8/pattern8.java
@@ -0,0 +1,14 @@
+package Pattern8;
+import java.util.*;
+public class pattern8 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+        for(int i = 1; i <= 5; i ++){
+            for(int j = 1; j <= 2*(5-i+1); j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
