d79dd68af46e043d49da547c37c93cbc3c317f9f
kr3shna <krishna2shende@gmail.com>
2024-08-18 15:27:54 +0530

    Pattern20

---
+++ patternPrinting/Pattern20/pattern20.java
@@ -0,0 +1,20 @@
+package Pattern20;
+import java.util.*;
+public class pattern20 {
+    public static void main(String[] args){
+        Scanner Scan = new Scanner(System.in);
+        int n = Scan.nextInt();
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < n; ++j){
+                if(i == 0 || i == n-1 || j == 0 || j == n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+}
